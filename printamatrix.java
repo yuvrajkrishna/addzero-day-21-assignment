56ef0f9a274ed2ba500379b9986f3a66706c96e6
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-18 20:06:34 +0530

    print matrix

---
+++ printamatrix.java
@@ -0,0 +1,11 @@
+public class printamatrix {
+    public static void main(String[] args) {
+        int arr [][] = {{10,20,30},{40,50,60},{70,80,90},{100,110,120},{130,140,150}};
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = 0 ; j < arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
